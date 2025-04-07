c48ba3b6fbc73369cb5126d56340e1c907f2d926
Maria <marfayaga@gmail.com>
2025-04-07 20:00:16 +0300

    create hello world react app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const rootNodeElement = document.querySelector('#main');
+const root = ReactDOM.createRoot(rootNodeElement);
+root.render(<div>Hello world</div>);
